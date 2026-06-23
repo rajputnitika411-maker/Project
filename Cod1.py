3a6fdc083012721e8259e9cb239462272f9ae228
Nitika Rajput <rajputnitika411@gmail.com>
2026-06-23 08:18:26 +0530

    python basics

---
+++ Cod1.py
@@ -0,0 +1,12 @@
+print("hello world")
+print(10+20)
+# comments and escape sequence characters
+# single line comment .hey guys less go!
+''' multiple line comment.'''
+print("hey myself nitika\n i m pursuing bca")
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)

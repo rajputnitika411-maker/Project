1a9f8f4621dd032d99f54dcfe105ae5624a599f7
Nitika Rajput <rajputnitika411@gmail.com>
2026-06-25 12:00:15 +0530

    cod2.py

---
+++ cod2.py
@@ -0,0 +1,19 @@
+#data types in python 
+a=1.1
+print(a)
+print("the type of a is",type(a))
+
+b="nitika rajput"
+print(b)
+c=23
+d=45
+print(c+d)
+print(c-d)
+print("the type of c is",type(c))
+print("the type of variable b is",type(b))
+list1=[1,2.3,[2,3],["nitu","whtsup"]]
+print(list1)
+tuple1=("nitika","rajput")
+print(tuple1)
+dir1={"name":"nitika"}
+print(dir1)

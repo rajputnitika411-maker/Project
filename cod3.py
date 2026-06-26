0780ef12e7b2c2ea86bbd83160f321e6ea0a37d7
Nitika Rajput <rajputnitika411@gmail.com>
2026-06-26 09:34:01 +0530

    calculator using python

---
+++ cod3.py
@@ -0,0 +1,11 @@
+#creating a calculator using python 
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+print("the value of",a,"+",b,"is:",a+b)
+print("the value of",a,"-",b,"is:",a-b)
+print("the value of",a,"*",b,"is:",a*b)
+print("the value of",a,"/",b,"is:",a/b)
+print("the value of",a,"/",b,"is:",a%b)
+
